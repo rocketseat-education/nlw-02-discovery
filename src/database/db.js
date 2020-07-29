51e05d4847e43c844f3f6a7c418d1e29dea4a767
maykbrito <maykbrito@gmail.com>
2020-07-28 23:16:29 -0300

    configuring database with examples

--- src/database/db.js
+++ src/database/db.js
@@ -1,17 +1,8 @@
-// importar a dependencia do sqlite3
-const sqlite3 = require("sqlite3").verbose()
 
-// criar o objeto que irá fazer operações no banco de dados
-const db = new sqlite3.Database(__dirname + "/database.db")
+const Database = require('sqlite-async')
 
-module.exports = db
-// utilizar o objeto de banco de dados, para nossas operações
-db.serialize(() => {
-
-    // com commandos SQL eu vou:
-
-    // // 1 Criar tabelas
-    db.run(`
+module.exports = Database.open(__dirname + '/database.db').then(db => 
+    db.exec(`
         CREATE TABLE IF NOT EXISTS teachers (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             name TEXT,
@@ -19,88 +10,19 @@ db.serialize(() => {
             whatsapp TEXT,
             bio TEXT
         );
-    `)
 
-    db.run(`
-        CREATE TABLE IF NOT EXISTS class_schedule (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            class_id INTEGER,
-            weekday INTEGER,
-            time_from INTEGER,
-            time_to INTEGER
-        );
-    `)
-
-    db.run(`
         CREATE TABLE IF NOT EXISTS classes (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             subject TEXT,
             cost TEXT,
             teacher_id INTEGER
         );
-    `)
 
-    // // 2 Inserir dados na tabela teachers
-    let query = `
-        INSERT INTO teachers (
-            name,
-            avatar,
-            whatsapp,
-            bio
-        ) VALUES (?,?,?,?);
-    `
-
-    let values = [
-        "Mayk Brito",
-        "https://avatars2.githubusercontent.com/u/6643122?s=460&u=1e9e1f04b76fb5374e6a041f5e41dce83f3b5d92&v=4",
-        "7899887766",
-        "Instrutor de Educação Física para iniciantes, minha missão de vida é levar saúde e contribuir para o crescimento de quem se interessar. Comecei a minha jornada profissional em 2001, quando meu pai me deu dois alteres de 32kg com a seguinte condição: \"Aprenda a fazer dinheiro com isso!\""
-    ]
-
-    function afterInsertData(err) {
-        if(err) {
-            return console.log(err)
-        }
-
-        console.log("Cadastrado com sucesso")
-        console.log(this)
-    }
-
-
-    db.run(query, values, afterInsertData)
-
-
-    // // insert data on classes table
-    // query = `INSERT INTO classes (
-    //     subject,
-    //     cost,
-    //     teacher_id
-    // ) VALUES (?,?,?);`
-
-    // values = [
-    //     'Programação WEB para iniciantes',
-    //     '100',
-    //     teacher_id
-    // ]
-
-
-    // 3 Consultar os dados da tabela
-    db.all(`SELECT * FROM teachers`, function(err, rows) {
-        if(err) {
-            return console.log(err)
-        }
-
-        console.log("Aqui estão seus registros: ")
-        console.log(rows)
-    })
-
-    // 4 Deletar um dado da tabela
-    // db.run(`DELETE FROM places WHERE id = ?`, [3], function(err) {
-    //     if(err) {
-    //         return console.log(err)
-    //     }
-
-    //     console.log("Registro deletado com sucesso!")
-    // })
-
-})
+        CREATE TABLE IF NOT EXISTS class_schedule (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            class_id INTEGER,
+            weekday INTEGER,
+            time_from INTEGER,
+            time_to INTEGER
+        );`)
+)
---
+++ src/database/test.js
@@ -0,0 +1,82 @@
+const db = require('./db')
+
+
+db.then(async(db) => {
+    await useDatabase(db)
+})
+
+async function useDatabase(db) {
+    
+    // Inserir dados na tabela teachers
+    const insertTeacher = () => {
+        const query = `
+            INSERT INTO teachers (
+                name,
+                avatar,
+                whatsapp,
+                bio
+            ) VALUES (?,?,?,?);
+        `
+
+        const values = [
+            "Mayk Brito",
+            "https://avatars2.githubusercontent.com/u/6643122?s=460&u=1e9e1f04b76fb5374e6a041f5e41dce83f3b5d92&v=4",
+            "7899887766",
+            "Instrutor de Educação Física para iniciantes, minha missão de vida é levar saúde e contribuir para o crescimento de quem se interessar. Comecei a minha jornada profissional em 2001, quando meu pai me deu dois alteres de 32kg com a seguinte condição: \"Aprenda a fazer dinheiro com isso!\""
+        ]
+
+        return db.run(query, values)
+    }
+
+    const insertedTeacher = await insertTeacher()
+    
+    // Inserir dados na tabela Classes
+    const insertClass = (teacher_id) => {
+        const query = `
+            INSERT INTO classes (
+                subject,
+                cost,
+                teacher_id
+            ) VALUES (?,?,?);
+        `
+
+        const values = [
+            'Programação WEB para iniciantes',
+            '100',
+            teacher_id
+        ]
+
+        return db.run(query, values)
+    }
+
+    const insertedClass = await insertClass(insertedTeacher.lastID)
+
+    // Inserir dados na tabela class_schedule
+    const insertClassSchedule = (values) => {
+        const query = `
+            INSERT INTO class_schedule (
+                class_id,
+                weekday,
+                time_from,
+                time_to
+            ) VALUES (?,?,?,?);
+        `
+        return db.run(query, values)
+    }
+
+    await insertClassSchedule([insertedClass.lastID,1,720,1220])
+    await insertClassSchedule([insertedClass.lastID,2,820,920])
+    await insertClassSchedule([insertedClass.lastID,0,20,220])
+    await insertClassSchedule([insertedClass.lastID,6,2000,2220])
+
+
+    // 3 Consultar os dados da tabela
+    const selectTeachers = await db.all(`SELECT * FROM teachers`)
+    console.log(selectTeachers)
+
+    const selectClasses = await db.all(`SELECT * FROM classes`)
+    console.log(selectClasses)
+
+    const selectClassSchedules = await db.all(`SELECT * FROM class_schedule`)
+    console.log(selectClassSchedules)
+}
